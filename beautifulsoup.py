8825d4b868045287afd5d27960c77dd8e2326b82
Ming <ming@college9-100-64-48-147.resnet.ucsc.edu>
2019-01-19 02:34:38 -0800

    more Alexa speakable

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -8,13 +8,14 @@ page_response = requests.get(page_link, timeout=5)
 # here, we fetch the content from the url, using the requests library
 page_content = BeautifulSoup(page_response.content, "html.parser")
 #we use the html parser to parse the url content and store it in a variable.
-#textContent = []
-#for i in range(0, 20):
-	#paragraphs = page_content.find_all("p")[i].text
-paragraphs = page_content.find(id="s-lg-content-38442577")
-#prettify()
-	# textContent.append(paragraphs)
-paragraphs.find("p").getText()
+
+paragraphs = page_content.find(id="s-lg-content-38442577").p.get_text()
+#paragraphs = quarter hours without all the tags
+
+paragraphs = paragraphs.replace("-", "to")
+paragraphs = paragraphs.replace("Mon", "Monday")
+paragraphs = paragraphs.replace("Thurs", "Thursday")
+
 print(paragraphs)
 # In my use case, I want to store the speech data I mentioned earlier.  so in this example, I loop through the paragraphs, 
 # and push them into an array so that I can manipulate and do fun stuff with the data.
