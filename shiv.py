148ba88fa4735f61637213ece1cb53231cf194b6
Shivansh Rustagi <shivrustagi221@gmail.com>
2019-01-19 09:24:51 -0800

    dining hall finished

--- shiv.py
+++ shiv.py
@@ -1,23 +1,41 @@
-import config
 from bs4 import BeautifulSoup as bs
 from datetime import datetime
-import googlemaps
 import requests
-import json
 
 
-# gmaps = googlemaps.Client(key=config.maps_key)
-# geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-# print(geocode_result)
-
-def check_open(dining_hall_name, menu_time):
+def check_open(dining_hall_name):
+	time = datetime.now()
+	time_seconds = (time - time.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
+	weekend = time.weekday() == 5 or time.weekday() == 6
+	if weekend and (dining_hall_name == "Porter/Kresge" or dining_hall_name == "Crown/Merrill"):
+		return False
+	elif weekend:
+		if time_seconds < 25200 or time_seconds > 82800:
+			return False
+	else:
+		if time_seconds < 23400 or time_seconds > 82800:
+			return False
 	return True
 
 
-def get_meal_time():
+
+def get_meal_time(dining_hall_name):
 	time = datetime.now()
 	time_seconds = (time - time.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
-	print(time_seconds)
+	if time.weekday() == 5 or time.weekday() == 6:
+		if time_seconds > 25200 and time_seconds < 36000:
+			return "Breakfast"
+		elif time_seconds > 36000 and time_seconds < 61200:
+			return "Lunch"
+		elif time_seconds > 61200 and time_seconds < 82800:
+			return "Dinner"
+	else:
+		if time_seconds > 23400 and time_seconds < 41400:
+			return "Breakfast"
+		elif time_seconds > 41400 and time_seconds < 61200:
+			return "Lunch"
+		elif time_seconds > 61200 and time_seconds < 82800:
+			return "Dinner"
 
 
 def scrape_dining_hall(dining_hall_name, menu_time = None):
@@ -30,9 +48,9 @@ def scrape_dining_hall(dining_hall_name, menu_time = None):
 		"Porter/Kresge": "https://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=15&locationName=Porter+Kresge+Dining+Hall&sName=&naFlag="
 	}
 	if not menu_time:
-		menu_time = get_meal_time()
-	if not check_open(dining_hall_name, menu_time):
-		return ("Sorry,", dining_hall_name, "is closed.")
+		menu_time = get_meal_time(dining_hall_name)
+	if not check_open(dining_hall_name):
+		return False
 	else:
 		soup = bs(requests.get(urls[dining_hall_name]).content, 'html.parser')
 		menu_items = soup.findAll("table", {"border": "0", "width": "100%", "height": "100%"})
@@ -44,9 +62,3 @@ def scrape_dining_hall(dining_hall_name, menu_time = None):
 					if len(label.string) != 1:
 						results.append(label.string)
 	return results
-
-url = "https://sho-prod.ucsc.edu/shs/common/login/stu_login.cfm"
-data = {"screenwidth": "0", "screenheight": "0", "LoginAction": "email", "username": config.username, "password": config.password, "login": "Log In"}
-r = requests.post(url = url, data = data)
-print(r.text) 
-print(scrape_dining_hall("9/10", "Dinner"))
