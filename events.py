7ec60a02f158a9455ede27033a1cc1e4390a2f9a
Shivansh Rustagi <shivrustagi221@gmail.com>
2019-01-20 01:27:03 -0800

    updated stuff

---
+++ events.py
@@ -0,0 +1,13 @@
+import feedparser
+from datetime import datetime as dt
+import dateutil.parser
+
+def scrape_events(date):
+	results = []
+	feed = feedparser.parse("http://rssmix.com/u/8308287/rss.xml")
+	for item in feed["items"]:
+		if dateutil.parser.parse(item["date"]).date() == date.date():
+			results.append(item["title"])
+	return results
+
+print(scrape_events(dt(2019, 2, 15)))
